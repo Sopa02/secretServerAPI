2cf611b4ae6b25bce01d609c03ae92fe9a8cfaad
Sopronyi Zoltán <70892729+Sopa02@users.noreply.github.com>
2021-09-10 13:15:31 +0200

    'működik'

--- Src/api/urls.py
+++ Src/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('secret/', views.addSecret, name="add-secret"),
+    path('secret', views.addSecret, name="add-secret"),
     path('secret/<str:_hash>', views.getSecret, name="get-secret")
 ]
--- Src/api/views.py
+++ Src/api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from .models import Secret
 from .serializers import SecretSerializer
+from django.core import serializers
 from django.utils.crypto import get_random_string
 from datetime import datetime
 from datetime import timedelta
@@ -12,14 +14,19 @@ import pytz
 @api_view(['POST'])
 def addSecret(request):
     _hash = get_random_string(length=6)
-    secretText = request.data['secretText']
+    secretText = request.data['secret']
     createdAt = datetime.now()
     expireAfterViews = request.data['expireAfterViews']
-    expireAfter = request.data['expireAfter']   #hours
-    expiresAt = createdAt + timedelta(hours=int(expireAfter))
+    expireAfter = request.data['expireAfter']   #minutes
+    expiresAt = createdAt + timedelta(minutes=int(expireAfter))
     secret = Secret(_hash=_hash, secretText=secretText, createdAt=createdAt, remainingViews=expireAfterViews, expiresAt=expiresAt)
     secret.save()
-    return Response({'hash' : 'valami'})
+    
+    if request.META.get('HTTP_ACCEPT') == "application/json":
+        return HttpResponse(serializers.serialize("json",[secret]), content_type='application/json')
+    else:
+        if request.META.get('HTTP_ACCEPT') == "application/xml":
+            return HttpResponse(serializers.serialize("json",[secret]), content_type='application/xml')
 
 
 
@@ -32,9 +39,15 @@ def getSecret(request, _hash):
         if secret.remainingViews != 0 and secret.expiresAt.replace(tzinfo=utc) > now:
             secret.remainingViews -=1
             secret.save()
-            serialized_secret = SecretSerializer(secret, many=False)
-            return Response(serialized_secret.data)
+            if request.META.get('HTTP_ACCEPT') == "application/json":
+                serialized_secret = serializers.serialize("json", [Secret.objects.get(_hash=_hash)])
+                return HttpResponse(serialized_secret, content_type='application/json')
+            else:
+                if request.META.get('HTTP_ACCEPT') == "application/xml":
+                    serialized_secret = serializers.serialize("xml", [Secret.objects.get(_hash=_hash)])
+                    return HttpResponse(serialized_secret, content_type='application/xml')  
+
         else:
             return Response({'Message' : 'This secret has expired!'})
     except:
-        return Response('Something went wrong. Maybe a bad hash?')
+       return Response('Something went wrong. Maybe a bad hash?')
