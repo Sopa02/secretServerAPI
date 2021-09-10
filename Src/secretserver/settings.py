58e41fd30a1155158bcf265a1ca1c8137ca8a417
Sopronyi Zoltán <70892729+Sopa02@users.noreply.github.com>
2021-09-10 13:17:00 +0200

    'működik'

--- Src/secretserver/settings.py
+++ Src/secretserver/settings.py
@@ -28,7 +28,7 @@ with open('secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
